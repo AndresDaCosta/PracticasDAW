dcc58f0d68990ce7428b6dcee61b1d37ea3d77c9
jorge costa <jorge.costa.work@hotmail.com>
2018-03-23 21:54:43 +0100

    comments

--- Pac2_modulo3/CalculadoraMenu/Program.cs
+++ Pac2_modulo3/CalculadoraMenu/Program.cs
@@ -11,12 +11,19 @@ namespace CalculadoraMenu
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Por favor introduce el 1º valor: ");
-
+            //declaracion de variables
+            
             int Opcion = new int();
             int Resultado = new int();
+            
+
+            Console.WriteLine("Por favor introduce el 1º valor: ");
+
             String Num = Console.ReadLine();
-            int Num1 = int.Parse(Num);    // se convierte el string que entrega la consola en un int
+            int Num1 = int.Parse(Num); // se convierte el string que entrega la consola en un int
+
+
+            /*Salida del Menu en la consola*/
 
             Console.WriteLine("que funcion quieres \r");
             Console.WriteLine("1 - Suma \r");
@@ -24,6 +31,7 @@ namespace CalculadoraMenu
             Console.WriteLine("3 - Division \r");
             Console.WriteLine("4 - Multiplicacion \r");
 
+            //se recibe un numero de la opcion
             Opcion = int.Parse(Console.ReadLine());
 
             //se pide el segundo valor
@@ -32,14 +40,14 @@ namespace CalculadoraMenu
 
             String Num2 = Console.ReadLine();
             int Num2_2 = int.Parse(Num2);    // se convierte un el segundo valor en int.
-
             
-
             //opciones MENU
 
             switch (Opcion)
             {
+                /*Casos de la calculadora*/
 
+                //Suma
                 case 1:
 
                     Resultado = Num1 + Num2_2;
@@ -47,15 +55,15 @@ namespace CalculadoraMenu
                     Console.WriteLine(Num1 + " + " + Num2_2 + " = " + Resultado);
 
                     break;
-
+                // resta
                 case 2:
 
                     Resultado = Num1 - Num2_2;
 
-                    Console.WriteLine(Num1 + " + " + Num2_2 + " = " + Resultado);
+                    Console.WriteLine(Num1 + " - " + Num2_2 + " = " + Resultado);
 
                     break;
-
+                //Division
                 case 3:
 
                     Resultado = Num1 / Num2_2;
@@ -63,7 +71,7 @@ namespace CalculadoraMenu
                     Console.WriteLine(Num1 + " / " + Num2_2 + " = " + Resultado);
 
                     break;
-
+                //Multiplicacion
                 case 4:
 
                     Resultado = Num1 * Num2_2;
@@ -74,6 +82,7 @@ namespace CalculadoraMenu
                     
             }
 
+            //finalizacion
             Console.WriteLine("pulsa cualquier tecla para acbar: ");
             Console.ReadLine();
 
