f27bb36e6b6d9d312c01214328aa501867996006
jorge costa <andrescostaribeiro16@gmail.com>
2018-04-04 23:57:32 +0200

    comments

--- Pac3_modulo3/Multidimension/Program.cs
+++ Pac3_modulo3/Multidimension/Program.cs
@@ -11,14 +11,20 @@ namespace Multidimension
         static void Main(string[] args)
         {
 
+            /*Realiza un programa que tenga una matriz (array bidimensional) con los 100 primeros números. 
+             * Mostrará esta matriz marcando con una X los números que no son primos, de forma que, únicamente 
+             * se vean los números primos. */
+
+            //creamos la matriz para los numeros
             int[,] matriz = new int[10, 10];
-            int contador = new int();
+            int contador = new int(); //creamos un contador para un uso posterior
 
             for (int i = 0; i < 10; i++)
-            {
+            {   
+                //rellenado del lado X de la matriz
                 for(int x = 0; x < 10; x++)
                 {
-
+                    //formula para el rellenado de la matriz
                     matriz[i, x] = (i*10) + x;
 
                 }
