b267883c4c97ec21b82e93a1b73af1ba3eba9640
jorge costa <andrescostaribeiro16@gmail.com>
2018-04-22 16:53:39 +0200

    interface

--- Pac4_modulo3/HundirLaFlota/HundirLaFlota/Program.cs
+++ Pac4_modulo3/HundirLaFlota/HundirLaFlota/Program.cs
@@ -10,22 +10,51 @@ namespace HundirLaFlota
     {
         static void Main(string[] args)
         {
-            
-            for(int i = 0; i <= 10; i++)
+            /*programa hundir la flota*/
+
+            /*Output de la primera posicion*/
+          
+            for ( int i = 0; i <= 10; i++)
             {
+                Console.WriteLine("+---++---++---++---++---++---++---++---++---++---++----+");
+                for (int x = 0; x <= 10; x++)
+                {
+                    
+                    if ( i == 0)
+                    {
 
-                Console.WriteLine("------------------------------------------------");
-                Console.WriteLine("| " + i + "  |  " + "  |  " + "  |  " + "  |  " + "  |  " + "  |  " + "  |  " + "  |  " + " | " + "  |");
+                        Console.Write("| " + x + " |");    
+
+                    }
+                    else
+                    {
+                        if (x == 0)
+                        {
+                            Console.Write("| " + i + " |");
+                        }
+                        else
+                        {
+                            Console.Write("| " + " " + " |");
+                        }
+
+                        
+
+                    }
 
 
+                   
+                }
 
+                Console.WriteLine();
+                
+
+                
 
             }
-            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("+---++---++---++---++---++---++---++---++---++---++----+");
             Console.ReadLine();
 
-
-
+            
         }
     }
 }
