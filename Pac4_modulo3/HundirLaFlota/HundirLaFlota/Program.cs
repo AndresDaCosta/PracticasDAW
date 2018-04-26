4eac7a5df504da1c85590d3faa3d54fd67b63688
jorge costa <andrescostaribeiro16@gmail.com>
2018-04-26 20:08:17 +0200

    primer Barco :)

--- Pac4_modulo3/HundirLaFlota/HundirLaFlota/Program.cs
+++ Pac4_modulo3/HundirLaFlota/HundirLaFlota/Program.cs
@@ -18,7 +18,7 @@ namespace HundirLaFlota
             char[,] tablero = new char[11, 8];
             char[] letras = { 'A', 'B', 'C', 'D', 'F', 'G', 'H' };
             int count = 0;
-
+            int corB = 0;
             /*barcos*/
             /*longitud 5*/
             char[] barcoP = { 'P', 'P', 'P', 'P', 'P' };
@@ -28,9 +28,10 @@ namespace HundirLaFlota
             char[] barcoT = { 'T', 'T', 'T'};
             /*longitud 2*/
             char[] barcoR = { 'R', 'R' };
-
+            char[] barcoSE = { };
             /*opciones*/
             Boolean vertical = new Boolean();
+            Boolean horizontal = new Boolean();
             Boolean arriba = new Boolean();
             Boolean derecha = new Boolean();
 
@@ -93,29 +94,33 @@ namespace HundirLaFlota
             Console.WriteLine("4- Barco R longitud " + barcoR.Length);
 
             Console.WriteLine("Elije el barco que quieres con el numero del menu.");
-            for(int i = 1; i == 4; i++) { 
+            for(int i = 1; i <= 4; i++) { 
                 switch (int.Parse(Console.ReadLine()))
                 {
 
                     case 1:
                         Console.WriteLine("has elejido el barco P con longitud " + barcoP.Length);
+                        barcoSE = barcoP;
                         break;
                     case 2:
                         Console.WriteLine("has elejido el barco M con longitud " + barcoM.Length);
+                        barcoSE = barcoP;
                         break;
                     case 3:
                         Console.WriteLine("has elejido el barco T con longitud " + barcoT.Length);
+                        barcoSE = barcoT;
                         break;
                     case 4:
                         Console.WriteLine("has elejido el barco R con longitud " + barcoR.Length);
+                        barcoSE = barcoR;
                         break;
 
                 }
-        
+                
                 Console.WriteLine("Como lo quieres poner Vertical o Horizontal ?");
                 String option = Console.ReadLine();
 
-                if(option == "Vertical")
+                if(option == "vertical")
                 {
                     vertical = true;
                 }
@@ -124,31 +129,86 @@ namespace HundirLaFlota
                     vertical = false;
                 }
 
-                Console.WriteLine("inserta las cordenada inicial del barco");
+                Console.WriteLine("inserta las cordenada inicial del barco !LETRA EN MAYUSCULA!");
 
-                char cordenada = Convert.ToChar(Console.ReadLine()); 
+                String cordenada = Console.ReadLine();
+                char[] cordenadaCh = cordenada.ToArray();
 
-                if(vertical == true | vertical == false)
+                
+
+                if (vertical == true & horizontal == false)
                 {
 
-                    Console.WriteLine("Por favor selecciona si quieres que se ponga hacia arriba o hacia abajo");
+                    if (cordenadaCh.Length == 3)
+                    {
+                        corB = 10;
+                    }
+                    else{
+
+                        corB = int.Parse(cordenada.Substring(1));
+                        
+                    }
+
+                    Console.WriteLine("Quieres que se ponga hacia arriba o hacia abajo");
                     
                     if(Console.ReadLine() == "arriba")
                     {
-                        arriba = true;
+
+                        for(int t = 0; t < 8; t++)
+                        {
+                            
+                            if(cordenadaCh[0].Equals(letras[t]))
+                            {
+
+                                if (((t+1) - barcoSE.Length) >= 0)
+                                {
+                                    tablero[corB, (t + 2)] = barcoSE[0];
+
+                                    for(int u = 1; u <= (barcoSE.Length - 1); u++)
+                                    {
+                                        int p = (t+2) - u;
+
+                                        tablero[corB, p] = barcoSE[u];
+                                        
+                                    }
+                                    
+
+                                }
+                                else
+                                {
+
+                                }
+
+                            }
+                            else
+                            {
+
+                            }
+
+                        }
+
                     }
                     else
                     {
 
-                        arriba = false;
                     }
 
-                } 
+                }
                 else
                 {
-                    Console.WriteLine("Por favor selecciona si quieres ");
+                    Console.WriteLine("Quieres que se ponga hacia la derecha o hacia abajo");
 
+                    if (Console.ReadLine() == "derecha")
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
                 }
+               
+                
 
             }
 
