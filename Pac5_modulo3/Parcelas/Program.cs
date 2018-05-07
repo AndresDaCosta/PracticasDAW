ad09a9f98de7f242b8f937e705287ecee0247bd0
jorge costa <andrescostaribeiro16@gmail.com>
2018-05-07 16:12:26 +0200

    comments

--- Pac5_modulo3/Parcelas/Program.cs
+++ Pac5_modulo3/Parcelas/Program.cs
@@ -26,6 +26,8 @@ namespace Parcelas
             En el programa principal, se mostrará el número actual de metros cuadrados, y el
             coste total de la operación.*/
 
+
+            /*creamos las variables para la funcion del programa*/
             int mi_parcela = new int();
             int vend_parcela = new int();
             int prec_cerca = new int();
@@ -36,38 +38,45 @@ namespace Parcelas
             prec_cerca = 15;
             mi_parcela = 500;
 
-
+            // preguntamos al vendedor pasandole los valores
             Vendedor(ref mi_parcela, ref prec_cerca, ref vend_parcela, out parcela_total, out precio_venta, out precio_tot);
 
+            //imprimimos en pantalla el detalle de la operacion
             Console.WriteLine("el vecino te vendio " + vend_parcela + " metros cuadrados por un precio " + precio_venta + " euros");
 
             Console.WriteLine("tu parcela total sera de " + parcela_total + " metros cuadrados");
             Console.WriteLine("te has gastado en este proceso " + precio_tot + " euros en la compra de la parcela y el cercado del terreno que ya tenias");
 
+            //llamamos a la funcion para calcular el total detalle de las operaciones 
             Operacion_tot(ref parcela_total, ref precio_venta, ref prec_cerca, out total);
+            // imprimimos el resultado en pantalla
+            Console.WriteLine("el precio total de la operacion sera " + total + " esto incluye el precio del nuevo terreno y el cercado del terreno toral");
 
-            Console.WriteLine("el precio total de la operacion sera " + total + "esto incluye el precio del nuevo terreno y el cercado del terreno toral");
-
+            Console.ReadLine();
 
         }
 
+        /*funcion para calcular el precio de la parcela que nos venden y su tamaño recive por ref los valores de las variables anteriores*/
         static void Vendedor(ref int  mi_parcela, ref int prec_cerca, ref int vend_parcela, out double parcela_total, out double precio_venta, out double precio_tot )
         {
-
+            // pedimos al vendedor el tamaño de la parcela que quiere vender
             Console.WriteLine("Por favor introduce el numero de metros que quieres vender ");
             vend_parcela = int.Parse(Console.ReadLine());
 
+            //pedimos el precio al que lo quiere vender
             Console.WriteLine("Por favor introduce el precio e la venta de ese terreno");
             precio_venta = double.Parse(Console.ReadLine());
 
-
+            // calculamos cual seria el tamaño total de la parcela
             parcela_total = vend_parcela + mi_parcela;
 
+            // calculamos el precio total de cercar nuestra parcela + el de la compra de la nueva parcela
             precio_tot = (prec_cerca * mi_parcela) + precio_venta;
           
                       
 
         }
+        // funcion que calcula el precio total  de las operaciones
         static void Operacion_tot(ref double parcela_total, ref double precio_venta, ref int prec_cerca, out double total)
         {
 
--- Pac5_modulo3/Temperatura/Program.cs
+++ Pac5_modulo3/Temperatura/Program.cs
@@ -10,11 +10,14 @@ namespace Temperatura
     {
         static void Main(string[] args)
         {
+            // creamos las variables que usaremos
             String[] temperaturas = new String[5];
             Double media = new Double();
 
+            // llamamos a la funcion para rellenar el array d las tempretaturas
             Relleno(temperaturas);
 
+            //llamamos a la funcion  para calcular la media de las temperaturas
             Calculo_media(temperaturas, ref media);
 
             Console.WriteLine("la media de las temperaturas de los paises es: " + media);
@@ -25,6 +28,7 @@ namespace Temperatura
 
         static String[] Relleno(String[] array)
         {
+            // hacemos un bucle para rellenar el array 
             for(int i = 0; i < 5; i++)
             {
                 Console.WriteLine("introduce la Pais y la temperatura con el siguiente formato Pais,temperatura");
@@ -33,30 +37,38 @@ namespace Temperatura
 
             }
 
-
+            // devolvemos el array relleno 
             return array;
             
         }
+        // funcion para calcular la media
         static void Calculo_media(String[] array, ref Double media)
         {
 
+            // creamos un array para guardar las temperaturas
             int[] Nums_temps = new int[5];
 
+            //bucle para sacar las temperaturas del array donde estan guardadas
             for(int i = 0; i < array.Length; i++)
             {
-
+                
+                // sacamos el String de las temperaturas
                 String temperatura = array[i];
 
+                // buscamos la coma que separa el nombre del pais y la temperatura
                 int index_com = temperatura.IndexOf(',');
 
+                // sacamos el numero de la temperatura
                 String numero = temperatura.Substring(index_com + 1);
 
+                // la metemos en el array
                 Nums_temps[i] = int.Parse(numero);
 
-                media = Nums_temps.Average();
+                
 
             }
-
+            // sacamos la media de las temperaturas
+            media = Nums_temps.Average();
 
         }
 
